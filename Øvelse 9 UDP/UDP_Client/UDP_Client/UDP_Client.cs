4a417a44a9b8d36bac65adbca200bb93a17d68ba
Richard Joakim Key <201500155@post.au.dk>
2016-10-31 15:31:29 +0100

    Øvelse 9 IKN

---
+++ Øvelse 9 UDP/UDP_Client/UDP_Client/UDP_Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDP_Client 
+{
+
+	static void Main(string[] args)
+	{
+		//Klargør UDP Client
+		int serverPort = 9000;
+		var client = new UdpClient (serverPort);
+		var socket = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+		//Tager imod serverIP og kommando
+		var serverIpAddress = IPAddress.Parse (args[0]);
+		string sendCommand = args[1];
+
+		//Sender kommando
+		byte[] sendbufferBytes = Encoding.ASCII.GetBytes (sendCommand);
+		var destination = new IPEndPoint (serverIpAddress, serverPort);
+		socket.SendTo (sendbufferBytes, destination);
+
+
+		//Venter på svar fra server og skriver resultat ud i konsoll.
+	    switch (args[1])
+	    {
+            case "U":
+            case "u":
+                byte[] uBytes = client.Receive(ref destination);
+                Console.WriteLine("Server uptime: {0}", Encoding.ASCII.GetString(uBytes, 0, uBytes.Length));
+	            break;
+
+	        case "L":
+            case "l":
+                byte[] lBytes = client.Receive(ref destination);
+                Console.WriteLine("Server load average: {0}", Encoding.ASCII.GetString(lBytes, 0, lBytes.Length));
+                break;
+
+            default:
+	            break;
+
+	    }
+		
+	}
+}
---
+++ Øvelse 9 UDP/UDP_Server/UDP_Server/UDP_Server.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Server
+{
+    public class UDP_Server 
+    {
+        private const int ServerPort = 9000;
+
+        private static void StartServer() 
+        {
+			//Klargør UDP server
+			var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var server = new UdpClient(ServerPort);
+            var serverInfo = new IPEndPoint(IPAddress.Any,ServerPort);
+
+            try 
+            {
+				//Venter på connection og kommando fra client
+                while (true) 
+                {
+                    Console.WriteLine("Waiting for command from  UDP client");
+                    byte[] bytes = server.Receive(ref serverInfo);
+                    var receivedCommand = Encoding.ASCII.GetString(bytes,0,bytes.Length);
+                    Console.WriteLine("UDP server has received the command: " + receivedCommand + " from " + serverInfo.Address);
+
+                    //Sender servers connection detaljer
+                    var responseEp = new IPEndPoint(serverInfo.Address, ServerPort);
+
+					//Sender loadaverage eller uptime efter modtaget karakter
+                    switch (receivedCommand)
+                    {
+                        case "U":
+                        case "u":
+                            using (var sr = new StreamReader ("/proc/uptime"))
+                            {
+                                var line = sr.ReadToEnd();
+                                Console.WriteLine("Sending server uptime: " + line);
+                                byte[] sendUbuf = Encoding.ASCII.GetBytes(line);
+
+                                socket.SendTo(sendUbuf, responseEp);
+                            }
+                            break;
+                        case "L":
+                        case "l":
+                            using (var sr = new StreamReader ("/proc/loadavg"))
+                            {
+                                var line = sr.ReadToEnd();
+                                Console.WriteLine("Sending server load average: " + line);
+
+                                byte[] sendLbuf = Encoding.ASCII.GetBytes(line);
+                                socket.SendTo(sendLbuf, responseEp);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } 
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                server.Close();
+            }
+        }
+
+		static int Main() 
+        {
+			StartServer();
+
+            return 0;
+        }
+    }
+}
